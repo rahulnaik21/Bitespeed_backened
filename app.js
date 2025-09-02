3d1b7235bf7ce8faf305969c2ac85f23d69601e6
rahulnaik21 <sabavathrahulnaik21@gmail.com>
2025-09-02 17:09:11 +0530

    Added cors middleware for frontend integration

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require("express");
+const cors = require("cors");
+app.use(cors());
+
+
 const bodyParser = require("body-parser");
-// const { PrismaClient } = require("@prisma/client");
+
 const identify = require("./identify");
 
 const app = express();
-// const prisma = new PrismaClient();
+
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
   res.send("BiteSpeed Backend is running");
+
 });
 
 app.post("/identify", identify);
